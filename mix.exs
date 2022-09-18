e3c1f50b0779b8cb076d5e91675408603ee0c95c
mrkirby153 <mr.austinwhyte@gmail.com>
2022-09-17 21:59:17 -0700

    Bootstrap project

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TaskScheduler.MixProject do
     [
       app: :task_scheduler,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,8 +21,10 @@ defmodule TaskScheduler.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:amqp, "~> 3.1"},
+      {:myxql, "~> 0.6.2"},
+      {:distillery, "~> 2.1"},
+      {:toml, "~> 0.6.1"}
     ]
   end
 end
---
+++ shell.nix
@@ -0,0 +1,14 @@
+{
+    pkgs ? import (
+        builtins.fetchTarball {
+            name = "nixpkgs-unstable-2022-09-17";
+            url = "https://github.com/nixos/nixpkgs/archive/9c2a7cc09d66fb7ffbc9609abe4d6d1521834152.tar.gz";
+            sha256 = "1rx2g6qg5gyavm7k7ssxqi12qxwwjshd8z0ca3sgba15ynda7l1q";
+        }
+    ) {}
+}:
+    pkgs.mkShell {
+        buildInputs = with pkgs; [
+            elixir
+        ];
+    }
