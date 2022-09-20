9e34eeede84a72a98f87ba762d431c02d65d0768
mrkirby153 <mr.austinwhyte@gmail.com>
2022-09-20 01:10:36 -0700

    Don't run breaking as a pre-commit hook

--- hooks/proto_breaking.sh
+++ hooks/proto_breaking.sh
@@ -3,8 +3,8 @@ set -e
 
 if [ -z "$IN_NIX_SHELL" ]; then
     echo "Re-executing in nix-shell"
-    exec nix-shell --command "$0 $@"
+    nix-shell --command "$0 $@"
 else
     branch=$(git rev-parse --abbrev-ref HEAD)
-    buf breaking --against ".git#branch=$branch"
+    buf breaking --against ".git#branch=$branch" --debug
 fi
--- hooks/proto_lint.sh
+++ hooks/proto_lint.sh
@@ -2,7 +2,7 @@
 set -e
 if [ -z "$IN_NIX_SHELL" ]; then
     echo "Re-executing in nix-shell"
-    exec nix-shell --command "$0 $@"
+    nix-shell --command "$0 $@"
 else
     pushd proto > /dev/null
     buf lint
