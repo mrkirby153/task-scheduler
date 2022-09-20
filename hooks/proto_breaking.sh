0ee58ba0697d1dd7fa24fdd1667546d1b6672f36
mrkirby153 <mr.austinwhyte@gmail.com>
2022-09-20 00:58:38 -0700

    Re-execute hooks in nix-shell when run outside

--- hooks/proto_breaking.sh
+++ hooks/proto_breaking.sh
@@ -1,4 +1,10 @@
 #!/bin/env bash
 set -e
-branch=$(git rev-parse --abbrev-ref HEAD)
-buf breaking --against ".git#branch=$branch"
+
+if [ -z "$IN_NIX_SHELL" ]; then
+    echo "Re-executing in nix-shell"
+    exec nix-shell --command "$0 $@"
+else
+    branch=$(git rev-parse --abbrev-ref HEAD)
+    buf breaking --against ".git#branch=$branch"
+fi
--- hooks/proto_lint.sh
+++ hooks/proto_lint.sh
@@ -1,5 +1,10 @@
 #!/bin/env bash
 set -e
-pushd proto > /dev/null
-buf lint
-popd > /dev/null
+if [ -z "$IN_NIX_SHELL" ]; then
+    echo "Re-executing in nix-shell"
+    exec nix-shell --command "$0 $@"
+else
+    pushd proto > /dev/null
+    buf lint
+    popd > /dev/null
+fi
